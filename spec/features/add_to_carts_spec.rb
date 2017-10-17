1450d9baafc619f6457e828944581e28720e4527
Bryan Hunter <bdhunter3141@gmail.com>
2017-10-17 18:10:41 +0000

    Added test for adding product to cart.

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor clicks on product name", type: :feature, js: true do
+RSpec.feature "Visitor clicks Add To Cart button", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,16 +16,16 @@ RSpec.feature "Visitor clicks on product name", type: :feature, js: true do
     end
   end
 
-  scenario "They are taken to the product page" do
+  scenario "The cart total number increases by 1" do
     # ACT
     visit root_path
-    page.first('.product').find_link('Details').click
+    page.first('.product').find_link('Add').click
 
     # DEBUG
     save_screenshot
 
     # VERIFY
-    expect(page).to have_content('Customer Rating')
+    cart_message = page.find :link, href: '/cart'
+    expect(cart_message).to have_text('My Cart (1)')
   end
 end
-
